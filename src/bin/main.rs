4369726bc786b2bea3d3d9cc856716b52d2d4adb
casibbald <casibbald@gmail.com>
2025-01-25 18:07:05 +0200

    Additional components to kubecontroller to process docs from example, but running on top of loco-rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,18 @@
 use loco_rs::cli;
-use yapp::app::App;
+use yapp::{
+    app::App,
+    controllers::{kubecontroller::run, lib::kubecontroller::State, telemetry},
+};
 
 
 #[tokio::main]
 async fn main() -> loco_rs::Result<()> {
-    cli::main::<App>().await
+    cli::main::<App>().await.expect("TODO: panic message");
+
+    telemetry::init().await;
+
+    // Initialize Kubernetes controller state
+    let state = State::default();
+    run(state.clone()).await; // Ensure `run` is awaited
+    Ok(())
 }
