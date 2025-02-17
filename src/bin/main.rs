0ad4119322783491ea9f0242e0e83f0482ec08d1
casibbald <casibbald@gmail.com>
2025-02-17 09:59:06 +0200

    Telemetry initialised, controller running and reconcile logs streaming
    bypass cli, loco starts, controller does not start

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,4 @@
-use loco_rs::cli;
+#[allow(unused_imports)]
 use yair::{
     app::App,
     controllers::{kubecontroller::run, telemetry},
@@ -7,12 +7,42 @@ use yair::{
 
 #[tokio::main]
 async fn main() -> loco_rs::Result<()> {
-    cli::main::<App>().await.expect("TODO: panic message");
+    let loco_rs_handle = tokio::spawn(async {
+        if let Err(e) = run_loco_rs().await {
+            eprintln!("Error in loco_rs: {e:?}");
+        }
+    });
 
     telemetry::init().await;
+    let kubecontroller_handle = tokio::spawn(async {
+        if let Err(e) = run_kubecontroller().await {
+            eprintln!("Error in loco_rs: {e:?}");
+        }
+    });
 
-    // Initialize Kubernetes controller state
+    // Await all tasks to complete
+    let _ = tokio::try_join!(kubecontroller_handle, loco_rs_handle)?;
+
+    Ok(())
+}
+
+async fn run_kubecontroller() -> Result<(), Box<dyn std::error::Error>> {
     let state = State::default();
-    run(state.clone()).await; // Ensure `run` is awaited
+    run(state.clone()).await;
+    Ok(())
+}
+
+async fn run_loco_rs() -> loco_rs::Result<()> {
+    println!("Starting loco_rs...");
+
+    let start_mode = loco_rs::boot::StartMode::ServerOnly;
+    let environment = loco_rs::environment::Environment::Development;
+    let boot_result = loco_rs::boot::create_app::<App>(start_mode, &environment).await?;
+
+    let server_params = loco_rs::boot::ServeParams {
+        port: 8080,
+        binding: "localhost".to_string(),
+    };
+    loco_rs::boot::start::<App>(boot_result, server_params, true).await?;
     Ok(())
 }
--- src/bin/tool.rs
+++
@@ -1,9 +0,0 @@
-use loco_rs::cli;
-use yair::app::App;
-
-pub mod crdgen;
-
-#[tokio::main]
-async fn main() -> loco_rs::Result<()> {
-    cli::main::<App>().await
-}
